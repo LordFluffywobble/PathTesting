895b6b4d766a9f282f22f75430583bbcd5c4381e
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-09-04 09:53:21 +0200

    GREEN: The shortespath test is now working. It had to return a double, not void

--- PathFinder/PathFinder.cs
+++ PathFinder/PathFinder.cs
@@ -14,8 +14,19 @@ public class PathFinders
     public void UpdateCoordinates(double x, double y, double z) => 
         _ = PathHistory.Count > 0 ? PathHistory[PathHistory.Count - 1] = new Coordinates(x, y, z) : default;
     
-    public void ShortestPathTest(double x, double y, double z)
+    public double ShortestPathTest()
     {
-        
+
+        if (PathHistory.Count < 2) 
+            return 0;
+        var path1 = PathHistory[0];
+        var path2 = PathHistory[1];
+
+        double dx = path2.X - path1.X;
+        double dy = path2.Y - path1.Y;
+        double dz = path2.Z - path1.Z;
+
+        return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+
     }
 }
